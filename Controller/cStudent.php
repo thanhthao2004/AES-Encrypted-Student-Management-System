bfd2dcbda2a92c6b94adf3be818bc7ec97f44174
tran thi thanh thao <131161263+thanhthao2004@users.noreply.github.com>
2025-03-14 10:08:32 +0700

    Thêm hàm get và delete ở model và controller. Sau đó chỉnh sửa view/DanhSachSV.php, xu ly ham delete o XoaSV.php

--- Controller/cStudent.php
+++ Controller/cStudent.php
@@ -1,21 +1,7 @@
 <?php 
     include_once('Model/mStudent.php');
     class cStudent{
-        public function addStudent($mssv, $hoTen, $ngaySinh, $gioiTinh, $lopDN) {
-            $p = new clsStudent();
-            $tblSinhvien = $p->addStudent($mssv, $hoTen, $ngaySinh, $gioiTinh, $lopDN);
-            if($tblSinhvien == 2){
-                header("Location: index.php?act=themSV&status=exist");// MSSV đã tồn tại
-            } else if($tblSinhvien == 5){
-                // echo "<script>alert('Lỗi kết nối!')</script>";
-                header("Location: index.php?act=themSV&status=error_db");
-            }
-            else if($tblSinhvien == 3){
-                header("Location: index.php?act=themSV&status=success");// Thêm SV thành công
-            } else {
-                // echo "<script>alert('Không thể thêm sinh viên!')</script>";
-                header("Location: index.php?act=themSV&status=fail");// Không thể thêm SV
-            }
+        public function addStudent() {
         }
         public function updateStudent() {
 
